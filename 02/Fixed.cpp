0ae6f9b661fdb48070ed139c9ee0d94d897518f5
YusufLisawi <yusufisawi@gmail.com>
2023-08-08 22:49:34 +0100

    02

---
+++ 02/Fixed.cpp
@@ -0,0 +1,17 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed() {
+}
+
+Fixed::~Fixed() {
+}
+
+Fixed::Fixed(const Fixed& other) {
+}
+
+Fixed& Fixed::operator=(const Fixed& other) {
+	if (this != &other) {
+		// Copy data here
+	}
+	return *this;
+}
---
+++ 02/Fixed.hpp
@@ -0,0 +1,12 @@
+#ifndef Fixed_HPP
+#define Fixed_HPP
+
+class Fixed {
+public:
+	Fixed();
+	~Fixed();
+	Fixed(const Fixed& other);
+	Fixed& operator=(const Fixed& other);
+};
+
+#endif
