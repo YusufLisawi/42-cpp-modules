cc155e3e9a6dc40b06f6903e7fb63c8daa0ac14e
YusufLisawi <yusufisawi@gmail.com>
2023-09-28 22:22:40 +0100

    06/ex00

--- 06/ex00/ScalarConverter.cpp
+++ 06/ex00/ScalarConverter.cpp
@@ -6,13 +6,11 @@
 /*   By: yelaissa <yelaissa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 21:32:51 by yelaissa          #+#    #+#             */
-/*   Updated: 2023/09/27 21:09:00 by yelaissa         ###   ########.fr       */
+/*   Updated: 2023/09/28 22:17:59 by yelaissa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
-
-char ScalarConverter::type = '\0';
 
 ScalarConverter::ScalarConverter() {
     // std::cout << "Constructor called for ScalarConverter"<< std::endl;
@@ -35,22 +33,68 @@ ScalarConverter& ScalarConverter::operator=(const ScalarConverter& other) {
     return *this;
 }
 
+
 void    ScalarConverter::convert(std::string str)
 {
     char    *endPtr;
+    double  d;
+
+    d = strtod(str.c_str(), &endPtr);  
+    if (str == "nan" || str == "nanf" || str == "+inf" || str == "+inf" || str == "-inf" || str == "-inf")
+	{
+		std::cout << "char : impossible" << std::endl;
+		std::cout << "int : impossible" << std::endl;
+		std::cout << "float : " << d << 'f' << std::endl;
+		std::cout << "double : " << d << std::endl;
+		return ;
+	}
     
-    strtod(str.c_str(), &endPtr);
-    // if (*endPtr != '\0' && str.length() == 1)
-    // {
-    //     str = static_cast<int>(str[0]);
-    //     std::cout << "char: '" << str << "'" << std::endl;  
-    // }
     if (*endPtr != '\0' && str[str.length() - 1] == 'f')
         str.erase(str.length() - 1);
+    if (*endPtr != '\0' && str.length() == 1)
+        d = static_cast<double>(str[0]);
 
-    std::cout << std::fixed << std::setprecision((str.find('.') != std::string::npos) ? str.length() - str.find('.') - 1 : 1);
-    printChar(str);
-    printInt(str);
-    printFloat(str);
-    // printDouble(str);
+    if ((*endPtr != '\0' && str.length() == 1) \
+        || (*endPtr != '\0' && str[str.length() - 1] == 'f') \
+        || (*endPtr == '\0' && str.find('.') != std::string::npos) \
+        || (*endPtr == '\0'))
+    {
+        std::cout << std::fixed << std::setprecision((str.find('.') != std::string::npos && str.find('.') != str.length() - 1) ? str.length() - str.find('.') - 1 : 1);
+        printChar(d);
+        printInt(d);
+        printFloat(d);
+        printDouble(d);
+    }
+    else
+    {
+        std::cout << "char: impossible" << std::endl;
+        std::cout << "int: impossible" << std::endl;
+        std::cout << "float: impossible" << std::endl;
+        std::cout << "double: impossible" << std::endl;
+    }
 }
+
+void    ScalarConverter::printChar(double d)
+{
+    std::cout << "char: " << ((d >= 0 && d <= 127) ? (std::isprint(d) ? std::string("'") + static_cast<char>(d) + "'" : "Non displayable") : "impossible") << std::endl;
+}
+
+void    ScalarConverter::printInt(double d)
+{
+    std::cout << "int: " << ((d > INT_MAX || d < INT_MIN) ? "impossible" : std::to_string(static_cast<int>(d))) << std::endl;
+}
+
+void    ScalarConverter::printFloat(double d)
+{
+    std::cout << "float: ";
+    if (d > FLT_MAX && d < FLT_MIN)
+        std::cout << "impossible" << std::endl;
+    else
+        std::cout << static_cast<float>(d) << "f" << std::endl;
+}
+
+void    ScalarConverter::printDouble(double d)
+{
+    std::cout << "double: " << d << std::endl;
+}
+
--- 06/ex00/ScalarConverter.hpp
+++ 06/ex00/ScalarConverter.hpp
@@ -6,7 +6,7 @@
 /*   By: yelaissa <yelaissa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 21:32:55 by yelaissa          #+#    #+#             */
-/*   Updated: 2023/09/27 20:34:04 by yelaissa         ###   ########.fr       */
+/*   Updated: 2023/09/28 22:21:06 by yelaissa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,13 +25,12 @@ private:
     ~ScalarConverter();
     ScalarConverter(const ScalarConverter& other);
     ScalarConverter& operator=(const ScalarConverter& other);
+    static void printChar(double d);
+    static void printInt(double d);
+    static void printFloat(double d);
+    static void printDouble(double d);
 public:
     static void convert(std::string str);
-    static char  type;
-    static void printChar(std::string str);
-    static void printInt(std::string str);
-    static void printFloat(std::string str);
-    static void printDouble(std::string str);
 };
 
 #endif
