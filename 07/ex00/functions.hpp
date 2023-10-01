9c9abd55361f350bb4ccd162b4b5671283a33c5a
YusufLisawi <yusufisawi@gmail.com>
2023-10-01 14:56:06 +0100

    Updates

---
+++ 07/ex00/functions.hpp
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   functions.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yelaissa <yelaissa@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/01 14:16:59 by yelaissa          #+#    #+#             */
+/*   Updated: 2023/10/01 14:20:31 by yelaissa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FUNCTIONS_HPP
+# define FUNCTIONS_HPP
+
+# include <iostream>
+
+template <typename T>
+void swap(T &a, T &b)
+{
+    T tmp = a;
+    a = b;
+    b = tmp;
+}
+
+template <typename T>
+T min(T a, T b)
+{
+    return (a < b ? a : b);
+}
+
+template <typename T>
+T max(T a, T b)
+{
+    return (a > b ? a : b);
+}
+
+#endif
---
+++ 07/ex00/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yelaissa <yelaissa@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/01 14:20:36 by yelaissa          #+#    #+#             */
+/*   Updated: 2023/10/01 14:55:45 by yelaissa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "functions.hpp"
+
+int main( void )
+{
+    int a = 2;
+    int b = 3;
+
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    ::swap(a, b);
+    std::cout << "a = " << a << ", b = " << b << std::endl;
+    std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+    std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+    std::string c = "chaine1";
+    std::string d = "chaine2";
+    ::swap(c, d);
+    std::cout << "c = " << c << ", d = " << d << std::endl;
+    std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
+    std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
+    return 0;
+}
