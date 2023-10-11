93a128d21e86c0ec9235440c54e4c39bf1081cbb
YusufLisawi <yusufisawi@gmail.com>
2023-10-11 20:24:22 +0100

    Updates

--- 08/ex02/MutantStack.hpp
+++ 08/ex02/MutantStack.hpp
@@ -1,13 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yelaissa <yelaissa@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/11 18:58:23 by yelaissa          #+#    #+#             */
+/*   Updated: 2023/10/11 19:37:56 by yelaissa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #pragma once
 
 #include <iostream>
+#include <stack>
+#include <deque>
 
-class MutantStack 
+template <class T, class Container = std::deque<T> >
+class MutantStack : public std::stack<T>
 {
 public:
-    MutantStack();
-    ~MutantStack();
-    MutantStack(const MutantStack& other);
-    MutantStack& operator=(const MutantStack& other);
+    MutantStack() : std::stack<T>() {};
+    ~MutantStack() {};
+    MutantStack(const MutantStack& other) : std::stack<T>() { 
+        *this = other;
+    };
+    MutantStack& operator=(const MutantStack& other) { 
+        (void)other;
+        return *this;
+    };
+    typedef typename Container::iterator iterator;
+    iterator begin() { 
+        return std::stack<T>::c.begin();
+    };
+    iterator end() {
+        return std::stack<T>::c.end();
+    };
 };
 
---
+++ 08/ex02/main.cpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yelaissa <yelaissa@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/11 17:21:41 by yelaissa          #+#    #+#             */
+/*   Updated: 2023/10/11 20:21:03 by yelaissa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "MutantStack.hpp"
+#include <iostream>
+
+int main()
+{
+    MutantStack<int> stack;
+
+    stack.push(1);
+    stack.push(2);
+    stack.push(3);
+    stack.push(4);
+    stack.push(5);
+
+    std::cout << "Elements in stack: ";
+    for (MutantStack<int>::iterator it = stack.begin(); it != stack.end(); ++it)
+    {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "Top element: " << stack.top() << std::endl;
+
+    stack.pop();
+    std::cout << "Elements in stack after pop(): ";
+    for (MutantStack<int>::iterator it = stack.begin(); it != stack.end(); ++it)
+    {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "Size of stack: " << stack.size() << std::endl;
+
+    return 0;
+}
